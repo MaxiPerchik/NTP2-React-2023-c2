3d3f4fdbac93253c876a7a9c6cfde4d00fce3524
Pablo Fernandez Hinojosa <ppfernandez@hiberus.com>
2023-09-18 20:03:45 -0300

    useSate

--- src/components/User/User.jsx
+++ src/components/User/User.jsx
@@ -1,8 +1,15 @@
 import './User.css';
+import { useState } from 'react';
 
 const User = (props) =>{
-     return (
-        <li className='user-item'>
+   let [contador, setContador] = useState(0);
+
+
+   const HandlerClick_User = () => {
+      setContador(contador + 1);
+   }  
+   return (
+        <li className='user-item' onClick={HandlerClick_User}>
             <div className='card user-item__content'>
                <a href="#">
                   <div className='user-item__image avatar'>
@@ -11,6 +18,7 @@ const User = (props) =>{
                   <div className='user-item__info'>
                      <h2>{props.UserName}</h2>
                      <h3>{props.Title}</h3>
+                     <h3>{contador}</h3>
                   </div>
                </a> 
             </div>
